074be5fa9e3e5058b1d67f5dd3e5787c7ab4d8bb
Daniel Cargan <daniel@3wm.dkl>
2022-02-07 21:32:15 +0100

    Initial commit

---
+++ src/js/modules/example.js
@@ -0,0 +1,31 @@
+/**
+ * Example JavaScript module
+ */
+
+console.log("Hello, world!");
+
+console.log("efefef");
+// var Example = (function($) {
+
+//   function initExample() {
+//     // do something
+//   }
+
+//   function init() {
+
+//     // return early if no corresponding module present
+//     if (!$('.Example').length) {
+//       return;
+//     }
+
+//     initExample();
+
+//   }
+
+//   return {
+//     init: init
+//   };
+
+// }(jQuery));
+
+// jQuery(document).ready(Example.init);
